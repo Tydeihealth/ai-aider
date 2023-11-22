4195cd3f0fcb19a9f38c38464c1275116e877eb4
Bart Cant <bart@tydei.io>
2023-11-22 12:22:57 -0500

    aider: Create an HTML input field and a button, and add an event listener to the button to display "Hello " followed by the input field's value when clicked.
    # Aider chat conversation:
    
    ASSISTANT: To fulfill this request, we need to modify `hello.js` to:
    
    1. Create an HTML input field and a button.
    2. Add an event listener to the button to display "Hello " followed by the input field's value when clicked.
    
    Here is the *SEARCH/REPLACE* block:
    
    hello.js
    ```javascript
    <<<<<<< SEARCH
    console.log("Hello World");
    // code: create an input field called "Name". Once you enter and click a button then the UI displays "hello <name>"
    =======
    // Create an input field and a button
    let input = document.createElement('input');
    input.id = 'name';
    let button = document.createElement('button');
    button.innerHTML = 'Click me';
    
    // Append the input field and button to the body
    document.body.appendChild(input);
    document.body.appendChild(button);
    
    // Add an event listener to the button
    button.addEventListener('click', function() {
        let name = document.getElementById('name').value;
        console.log('Hello ' + name);
    });
    >>>>>>> REPLACE
    ```

--- hello.js
+++ hello.js
@@ -1,2 +1,15 @@
-console.log("Hello World");
-// code: create an input field called "Name". Once you enter and click a button then the UI displays "hello <name>"
+// Create an input field and a button
+let input = document.createElement('input');
+input.id = 'name';
+let button = document.createElement('button');
+button.innerHTML = 'Click me';
+
+// Append the input field and button to the body
+document.body.appendChild(input);
+document.body.appendChild(button);
+
+// Add an event listener to the button
+button.addEventListener('click', function() {
+    let name = document.getElementById('name').value;
+    console.log('Hello ' + name);
+});
